8335e9b00158b50939bb70f54553e23fd016930a
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-02-05 20:31:26 +1100

    Start to lay out the code for comprehensions

--- notations.py
+++ notations.py
@@ -1,10 +1,13 @@
 import enum
 import dis
 from collections import deque
+import types
 
 
 FOR_ITER = 'FOR_ITER'
 POP_BLOCK = 'POP_BLOCK'
+LOAD_CONST = 'LOAD_CONST'
+MAKE_FUNCTION = 'MAKE_FUNCTION'
 
 
 class NOTATION_TYPES(enum.IntEnum):
@@ -26,14 +29,38 @@ NOTATION_REPR = {
 
 
 def notation(func, debug=False):
+    """
+    Get the Big-O notation for a Python code object.
+
+    :param func: The code object to analyse
+    :type  func: ``types.CodeType``
+
+    :param debug: Show debug information
+    :type  debug: ``bool``
+
+    :return: Return the O-notation for a given code object
+    :rtype: :class:`NOTATION_TYPES`
+    """
+    if debug:
+        print(dis.code_info(func))
     bytecode = dis.Bytecode(func)
+    _nested_func = None  # Nested functions, e.g. lambdas and comprehensions
     max_loop_stack_depth = 0
+    extra_stacks = 0
     # setup the loop stacks as a deque
     loop_stacks = deque()
     for i in bytecode:
-        if i.opname == FOR_ITER:
+        if i.opname == FOR_ITER:  # basic for loop
             loop_stacks.append(i.argval)
-        elif i.opname == POP_BLOCK:
+        elif i.opname == LOAD_CONST and isinstance(i.argval, types.CodeType):
+            # Load code objects onto the stacks, evaluate the notation of the object
+            # and store locally if that function is loaded.
+            _nested_notation = notation(i.argval, debug=debug)
+        elif i.opname == MAKE_FUNCTION:
+            # Where there is a dynamically-generated function, add the loop-stack-depth of it.
+            if _nested_func:
+                extra_stacks += int(_nested_func)
+        elif i.opname == POP_BLOCK:  # pop back up the stack.
             offset = loop_stacks.pop()
             if offset != i.offset:  # Check the popped value is the offset of the pop_block
                 raise ValueError("Not implemented, maybe. Or maybe this is a bug.")
@@ -43,5 +70,5 @@ def notation(func, debug=False):
         if len(loop_stacks) > max_loop_stack_depth:
             max_loop_stack_depth = len(loop_stacks)
 
-    return NOTATION_TYPES(max_loop_stack_depth)
+    return NOTATION_TYPES(max_loop_stack_depth + extra_stacks)
 
