587a9ce6996e48fc660e3bddd24aa819541a2e20
Anthony Shaw <anthonyshaw@apache.org>
2019-02-05 16:03:01 +1100

    add tests

--- notations.py
+++ notations.py
@@ -3,59 +3,24 @@ import inspect
 import dis
 from collections import deque
 
+
 FOR_ITER = 'FOR_ITER'
 POP_BLOCK = 'POP_BLOCK'
 
-O_n = 'O(n)'
-O_n_power_2 = 'O(n^2)'
-O_n_power_n = 'O(n^n)'
+class NOTATION_TYPES:
+    O_1 = 'O(1)'
+    O_n = 'O(n)'
+    O_n_power_2 = 'O(n^2)'
+    O_n_power_n = 'O(n^n)'
 
 NOTATIONS = {
-    0: O_n,
-    1: O_n_power_2,
-    2: O_n_power_n,
+    0: NOTATION_TYPES.O_1,
+    1: NOTATION_TYPES.O_n,
+    2: NOTATION_TYPES.O_n_power_2,
+    3: NOTATION_TYPES.O_n_power_n,
     # etc..
 }
 
-
-def function1(arg1):
-    if arg1 == True:
-        return True
-    else:
-        return False
-
-
-def function2(arg1):
-    i = 0
-    for a in arg1:
-        i += 1
-    return i
-
-
-def function3(arg1, arg2):
-    i = 0
-    j = 0
-    for a in arg1:
-        for b in arg2:
-            i += 1
-            j += 1
-    return i, j
-
-
-def function4(arg1, arg2):
-    i = 0
-    j = 0
-    for a in arg1:
-        i += 1
-    
-    for b in arg2:
-        j += 1
-
-    return i, j
-
-
-def function5(arg1, arg2):
-    pass
 
 def notation(func):
     bytecode = dis.Bytecode(func)
@@ -75,8 +40,3 @@ def notation(func):
     # For now, just return the max stack depth
     return NOTATIONS[max_loop_stack_depth]
 
-print("Function 1 has a loop stack of ", notation(function1))
-print("Function 2 has a loop stack of ", notation(function2))
-print("Function 3 has a loop stack of ", notation(function3))
-print("Function 4 has a loop stack of ", notation(function4))
-print("Function 5 has a loop stack of ", notation(function5))
---
+++ test_notations.py
@@ -0,0 +1,49 @@
+import notations
+
+
+def test_1():
+    def function1(arg1):
+        if arg1 == True:
+            return True
+        else:
+            return False
+    assert notations.notation(function1) == notations.NOTATION_TYPES.O_1
+
+def test_2():
+    def function2(arg1):
+        i = 0
+        for a in arg1:
+            i += 1
+        return i
+    assert notations.notation(function2) == notations.NOTATION_TYPES.O_n
+
+
+def test_3():
+    def function3(arg1, arg2):
+        i = 0
+        j = 0
+        for a in arg1:
+            for b in arg2:
+                i += 1
+                j += 1
+        return i, j
+    assert notations.notation(function3) == notations.NOTATION_TYPES.O_n_power_2
+
+def test_4():
+    def function4(arg1, arg2):
+        i = 0
+        j = 0
+        for a in arg1:
+            i += 1
+        
+        for b in arg2:
+            j += 1
+
+        return i, j
+    assert notations.notation(function4) == notations.NOTATION_TYPES.O_n
+
+
+def test_5():
+    def function5(arg1, arg2):
+        pass
+    assert notations.notation(function5) == notations.NOTATION_TYPES.O_1
